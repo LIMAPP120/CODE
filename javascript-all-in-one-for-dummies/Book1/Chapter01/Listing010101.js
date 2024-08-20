b37fa7bb0bb46c61786572a580e4569bf9a7368a
LIMAPP120 <limapp120@gmail.com>
2024-08-21 04:23:22 +0800

    first commit

--- javascript-all-in-one-for-dummies/Book1/Chapter01/Listing010101.js
+++
@@ -1,7 +0,0 @@
-let normalName = "Chris";
-let javaScriptName = normalName + "Script";
-console.log("Your JavaScript Name is " + javaScriptName);
-
-let b = !!"JavaScript is awesome";
-console.log(b);
-console.log("llllllllll");
